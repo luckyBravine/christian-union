29e247439100ee6069b4ae8fed1ec972d7de1f4b
luckyBravine <luckybravine@gmail.com>
2023-06-17 13:01:08 +0300

    web responsiveness 100% complete

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e3c3a71d6af777982884c11e877b4a755ba4bcfae661e88bfdbc1c89e0fe0b6
-size 12240
+oid sha256:0c1adc9465865268a054bd7a76578184a0bb8e6dff28668919e671827cfad7ee
+size 13140
