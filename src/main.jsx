3737c66ba77d12b2437337d03a76b8cefdd3d374
Bravine M. Alusiola <98695541+luckyBravine@users.noreply.github.com>
2023-06-17 23:49:30 +0300

    making changes to package.json by uninstalling...

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { Provider } from "react-redux";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:ccbc63447b90eb1442eb34f2618c2f67617238d35cbb4f9d21586b5e3d96e8fb
+size 247
