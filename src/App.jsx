277bccf03fcc8f032d0e318d169fc684ca3cc988
Bravine M. Alusiola <98695541+luckyBravine@users.noreply.github.com>
2023-06-18 08:54:54 +0300

    making changes to the whole project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:599e5246dc3d58ebdcb5cb804883f021bdfe47714ae96c29cad844b84a86932f
-size 7230
+oid sha256:1fa3b902b619920ec3a780698bca7ce4880302d742d80749276dc8a1f887e2bb
+size 7241
