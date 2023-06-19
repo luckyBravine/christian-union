c3b5291a20d5651c6a12dd46ffee1f2685e62b4b
Bravine M. Alusiola <98695541+luckyBravine@users.noreply.github.com>
2023-06-19 08:47:48 +0300

    making changes to the whole project

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5643b6e418f02497c7bae4ffb5861124d1723463832c49df6e94d038b02e346
-size 13078
+oid sha256:a0dc485513c2513db92e3cafb3fe4be72ca6ea3914f7e8233427a22e1fee1a0c
+size 12999
