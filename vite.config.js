e971d19b5f6a78df3af99528ae3e6a1743dc9d38
Bravine M. Alusiola <98695541+luckyBravine@users.noreply.github.com>
2023-06-19 20:20:43 +0300

    making changes to the whole project vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:bd68e14ce0734605ef44fc9f5020ccd43f14936527274085f3fc407490a30606
+size 194
