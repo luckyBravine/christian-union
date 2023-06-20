c064a585d57c4165960c1d678493971bd122ed26
Bravine M. Alusiola <98695541+luckyBravine@users.noreply.github.com>
2023-06-20 21:48:24 +0300

    making changes to the whole project vite doc

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd68e14ce0734605ef44fc9f5020ccd43f14936527274085f3fc407490a30606
-size 194
+oid sha256:fcacffeaab52f52d90e039d33744fe6a3ca0abc7a25ba4ba628048712451ec4b
+size 219
